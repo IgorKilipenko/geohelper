2761ecf0ea8fe0eb3756fc6671a8b84cf217a930
IgorKilipenko <igorkili@mail.ru>
2023-10-25 20:55:21 +0700

    rm unused imports

--- IgorKL.ACAD3.Model/Commands/ToolsCmd.cs
+++ IgorKL.ACAD3.Model/Commands/ToolsCmd.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.Geometry;
+﻿using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Colors;
-using Autodesk.Civil.DatabaseServices;
-
-using IgorKL.ACAD3.Model.Helpers.SdrFormat;
-using wnd = System.Windows.Forms;
 
 namespace IgorKL.ACAD3.Model.Commands {
 
